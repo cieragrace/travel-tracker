e121f765dc4f05cf06165177d0c102f89bbac5c9
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-01-11 15:25:37 -0700

    Add get traveler flights function

--- src/traveler.js
+++ src/traveler.js
@@ -1,7 +1,14 @@
 class Traveler {
-  constructor(travelers) {
+  constructor(travelers, tripsData, destinationData) {
     this.travelers = travelers
+    this.tripsData = tripsData
+    this.destinationData = destinationData
   }
+
+  findTravelersFlights() {
+    return this.tripsData.trips.filter(trip => trip.userID === this.travelers.id)
+  }
+
 }
 
 export default Traveler;
