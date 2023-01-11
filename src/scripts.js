b2b65e5e8a42a169d4392c88bb58950a9064de59
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-01-11 12:50:20 -0700

    Add API function skeleton

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,16 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
+import Traveler from './traveler';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+function getAllData() {
+  Promise.all([getAPIData('travelers'), getAPIData('trips'), getAPIData('destinations')])
+    .then((data) => {
+      travelers = new Traveler(data[0])
+      trips = data[1]
+      destinations = data[2]
+      loadPage(())
+    })
+    .catch(err => console.log("To err is human", err))
+}
