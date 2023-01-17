a5b60f89f70ef8754327fcc26146b319c69b611c
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-01-17 09:44:37 -0700

    Add new event listener

--- src/scripts.js
+++ src/scripts.js
@@ -33,7 +33,7 @@ let destinationsDiv = document.querySelector('.list-destinations')
 let destinationCosts = document.querySelector(".list-destination-costs")
 let userNameInput = document.querySelector('#userName')
 let passwordInput = document.querySelector('#password')
-let displayTravelerName = document.querySelector('#displayName')
+let travelerName = document.querySelector('#userName')
 let tripsContainer = document.querySelector('.list-trips')
 
 //---------Event Listeners--------------
@@ -43,11 +43,7 @@ planButton.addEventListener("click", displayFututeTripTotals)
 loginButton.addEventListener("click", function (event) {
   event.preventDefault
   findUser()
-  // openTravelerPage()
-  // displayingTravelersFlights()
-  // displayPossibleDestinations()
 })
-// postTripButton.addEventListener("click", postNewTrip)
 
 //-----------Functions-------------------
 
@@ -72,7 +68,6 @@ function addUsername() {
 function findUser() {
   const validUser = travelers.data.travelers.find(traveler => {
     return traveler.userName === userNameInput.value
-    // loginPageError.classList.add("add")
   })
   console.log("Valid:", validUser)
   currentTraveler = new Traveler(validUser, trips, destinations)
@@ -80,16 +75,7 @@ function findUser() {
   openTravelerPage()
   displayingTravelersFlights()
   displayPossibleDestinations()
-  // return findUser
-
 }
-
-
-// function getTraveler(tripsData, destinationData) {
-//   let randomIndex = Math.floor(Math.random() * travelers.data.travelers.length)
-//   let randomUser = travelers.data.travelers[randomIndex - 1]
-//   currentTraveler = new Traveler(randomUser, tripsData, destinationData)
-// }
 
 function enableLoginButton() {
 if (userNameInput.value &&
@@ -105,29 +91,8 @@ if (userNameInput.value &&
 function openTravelerPage() {
   loginPage.classList.add('hidden')
   travelersPage.classList.remove('hidden')
+  travelerName.innerHTML += currentTraveler.travelers.name
   }
-
-function displayingTravelersFlights() {
-    const travelersTrips = currentTraveler.tripsData.trips.filter(trip => {
-      return trip.userID === currentTraveler.travelers.id
-    })
-  // let travelersTrips = currentTraveler.findTravelersFlights()
-  // console.log('travelersTrips', travelersTrips)
-  let destinationName = currentTraveler.getDestinationInfoPerID("destination")
-  trip1.innerHTML += `<li>${destinationName[0]}</li>`
-  trip1info.innerHTML += `<li>on ${travelersTrips[0].date}</li>
-  <li>with ${travelersTrips[0].travelers} people</li>
-  <li>for ${travelersTrips[0].duration} days</li>`
-  trip2.innerHTML += `<li>${destinationName[1]}</li>`
-  trip2info.innerHTML += `<li>on ${travelersTrips[1].date}</li>
-  <li>with ${travelersTrips[1].travelers} people</li>
-  <li>for ${travelersTrips[1].duration} days</li>`
-  trip3.innerHTML += `<li>${destinationName[2]}</li>`
-  trip3info.innerHTML += `<li>on ${travelersTrips[2].date}</li>
-  <li>with ${travelersTrips[2].travelers} people</li>
-  <li>for ${travelersTrips[2].duration} days</li>`
-  yearlyCost.innerHTML += `$${currentTraveler.getTripTotal()}`
-}
 
 function enablePlanButton() {
   if (pickedDate.value && numDays.value && numPeople.value) {
@@ -141,43 +106,49 @@ function enablePlanButton() {
  setInterval(enablePlanButton, 500);
 
 function displayPossibleDestinations() {
-  // const possibleTrips = currentTraveler.getDestinationsInfo("destination")
+  const travelersTrips = currentTraveler.tripsData.trips.filter(trip => trip.userID === currentTraveler.travelers.id)
   const possibleTrips = currentTraveler.destinationData.destinations
   const eachDest = possibleTrips.forEach(destination => {
-    const destinationContainer = document.createElement("div")
-    destinationContainer.classList.add("destinations")
-    console.log(destination)
-    destinationContainer.style.backgroundImage = `url('${destination.image}')`
-    const destinationName = document.createElement("p")
-    destinationName.classList.add("names-of-destinations")
-    destinationName.innerHTML += `${destination.destination}`
-    destinationContainer.appendChild(destinationName)
-    destinationsDiv.appendChild(destinationContainer)
+  const destinationContainer = document.createElement("div")
+  destinationContainer.classList.add("destinations")
+  console.log(destination)
+  destinationContainer.style.backgroundImage = `url('${destination.image}')`
+  const destinationName = document.createElement("p")
+  destinationName.classList.add("names-of-destinations")
+  destinationName.innerHTML += `${destination.destination}`
+  destinationContainer.appendChild(destinationName)
+  destinationsDiv.appendChild(destinationContainer)
+    })
+  }
+
+
+function displayingTravelersFlights() {
+  travelerName.innerHTML += currentTraveler.travelers.name
+  console.log(currentTraveler.travelers.name)
+  yearlyCost.innerHTML += `You've spent $${currentTraveler.getTripTotal()} exploring the world`
+  const thisTraveler = currentTraveler.findTravelersFlights()
+  const eachTrip = thisTraveler.forEach(trip => {
+    const foundDestination = currentTraveler.destinationData.destinations.find(destination => {
+      return destination.id === trip.destinationID
+    }) 
+    if (foundDestination) {
+      const tripBox = document.createElement("div")
+      tripBox.classList.add("trips-divs-container")
+      tripsContainer.appendChild(tripBox)
+      const tripBoxp = document.createElement("h2")
+      tripBoxp.classList.add("trips-divs")
+      tripBoxp.innerHTML += foundDestination.destination
+      console.log(foundDestination.destination)
+      tripBox.appendChild(tripBoxp)
+      const tripBoxUL = document.createElement("ul")
+      tripBoxUL.classList.add("trips-info")
+      tripBoxUL.innerHTML += `<li>on ${trip.date}</li>
+      <li>with ${trip.travelers} people</li>
+      <li>for ${trip.duration} days</li>`
+      tripBox.appendChild(tripBoxUL)
+    }
   })
 }
-
-// function displayingTravelersFlights() {
-//     const travelersTrips = currentTraveler.tripsData.trips.filter(trip => {
-//       return trip.userID === currentTraveler.travelers.id
-//   })
-//     const eachTrip = travelersTrips.forEach(trip => {
-//       // let destinationName = currentTraveler.getDestinationInfoPerID("destination")
-//       const tripBox = document.createElement("div")
-//     // tripBox.setAttribute("id", destination.id)
-//       tripBox.classList.add("trips-divs-container")
-//       tripsContainer.appendChild(tripBox)
-//       const tripBoxp = document.createElement("p")
-//       tripBoxp.classList.add("trips-divs")
-//       tripBoxp.innerHTML += `<li>${destinationName}</li>`
-//       tripBox.appendChild(tripBoxp)
-//       const tripBoxUL = document.createElement("ul")
-//       tripBoxUL.classList.add("trips-info")
-//       tripBoxUL.innerHTML += `<li>on ${travelersTrips[0].date}</li>
-//       <li>with ${travelersTrips.travelers} people</li>
-//       <li>for ${travelersTrips.duration} days</li>`
-//       tripBox.appendChild(tripBoxUL)
-//     })
-//   }
 
 function displayFututeTripTotals() {
   if (pickedDate.value && numDays.value && numPeople.value) {
@@ -225,11 +196,7 @@ function postNewTrip(event) {
   }
 
 function loadPage() {
-// addUsername()
 enableLoginButton()
-// displayPossibleDestinations()
 enablePlanButton()
 displayFututeTripTotals()
-// validateUser()
 }
-
